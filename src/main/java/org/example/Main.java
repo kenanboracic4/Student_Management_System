0482ef88e506d54665c56792b45fa4b89e1b89d0
kenanboracic4 <kenanboracic.dev@gmail.com>
2025-12-20 00:53:53 +0100

    add Main java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,47 +1,44 @@
 package org.example;
 
-import org.example.models.Student;
+import org.example.DbConnection;
+import org.example.repository.implementations.CourseRepository;
+import org.example.repository.implementations.EnrollmentRepository;
 import org.example.repository.implementations.StudentRepository;
+import org.example.repository.interfaces.CourseInterface;
+import org.example.repository.interfaces.EnrollmentInterface;
 import org.example.repository.interfaces.StudentInterface;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Optional;
+import org.example.services.CourseService;
+import org.example.services.EnrollmentService;
+import org.example.services.StudentService;
+import org.example.ui.ConsoleUI;
 
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
 
         try {
             DbConnection.initializeDatabase();
         } catch (RuntimeException e) {
-            System.err.println("FATALNA GREŠKA: Inicijalizacija baze nije uspjela. Aplikacija se gasi.");
+            System.err.println("FATALNA GREŠKA: Inicijalizacija baze nije uspjela.");
             System.exit(1);
         }
 
-        System.out.println("Baza uspješno inicijalizirana/tabele provjerene.");
 
-        // Korak 2: Test konekcije i pokretanje aplikacije
-        try (Connection conn = DbConnection.getConnection()) {
-
-            if (conn != null) {
-                System.out.println("✅ Konekcija je uspješno uspostavljena. Spremni za rad!");
-
-                StudentInterface studRepo =  new StudentRepository();
-
-                ArrayList<Student> studenti = studRepo.getAllStudents();
-
-                Optional<Student> pronadjeniStudent = studRepo.getStudentByIndex("473");
-                System.out.println(pronadjeniStudent);
+        StudentInterface studentRepo = new StudentRepository();
+        CourseInterface courseRepo = new CourseRepository();
+        EnrollmentInterface enrollmentRepo = new EnrollmentRepository();
 
 
-            } else {
-                System.err.println("Greska: Konekcioni objekat je null.");
-            }
+        StudentService studentService = new StudentService(studentRepo);
+        CourseService courseService = new CourseService(courseRepo);
+        EnrollmentService enrollmentService = new EnrollmentService(
+                (EnrollmentRepository) enrollmentRepo,
+                studentService,
+                courseService
+        );
 
-        } catch (SQLException e) {
-            System.err.println("❌ Greska pri radu s bazom: " + e.getMessage());
-        }
+
+        ConsoleUI ui = new ConsoleUI(studentService, courseService, enrollmentService);
+        ui.start();
     }
 }
